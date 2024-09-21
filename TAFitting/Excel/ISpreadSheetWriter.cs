103fcf1e7c26ed8c2928c4158c0a506852f2c622
ikuzak <k.kohzuki@gmail.com>
2024-09-22 03:55:56 +0900

    Add ISpreadSheetWriter interface to TAFitting.Excel
    Introduced a new internal interface `ISpreadSheetWriter` to the
    `TAFitting.Excel` namespace. The interface defines properties
    and methods for handling spreadsheet writing operations, including:
    - `Model`: A read-only property representing the fitting model.
    - `Parameters`: A read-write property for a list of parameter names.
    - `Times`: A read-write property for a list of time values.
    - `AddRow`: A method to add a row to the spreadsheet with a
      specified wavelength and parameters.
    - `Write`: A method to write the spreadsheet to a specified file path.

---
+++ TAFitting/Excel/ISpreadSheetWriter.cs
@@ -0,0 +1,19 @@
+﻿
+// (c) 2024 Kazuki KOHZUKI
+
+using TAFitting.Model;
+
+namespace TAFitting.Excel;
+
+internal interface ISpreadSheetWriter
+{
+    IFittingModel Model { get; init; }
+
+    IReadOnlyList<string> Parameters { get; set; }
+
+    IReadOnlyList<double> Times { get; set; }
+
+    void AddRow(double wavelength, IEnumerable<double> parameters);
+
+    void Write(string path);
+} // internal interface ISpreadSheetWriter
