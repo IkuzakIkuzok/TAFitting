9059b6d61bf7ce3522995f301053d4051eb3c7f9
ikuzak <k.kohzuki@gmail.com>
2024-09-22 01:39:29 +0900

    Add event handler for parameters table selection changes
    Introduce ParametersTableSelectionChangedEventHandler delegate
    and ParametersTableSelectionChangedEventArgs class in the
    TAFitting.Controls namespace. The event args class includes a
    Row property of type ParametersTableRow.

---
+++ TAFitting/Controls/ParametersTableSelectionChangedEventArgs.cs
@@ -0,0 +1,16 @@
+﻿
+// (c) 2024 Kazuki KOHZUKI
+
+namespace TAFitting.Controls;
+
+internal delegate void ParametersTableSelectionChangedEventHandler(object? sender, ParametersTableSelectionChangedEventArgs e);
+
+internal sealed class ParametersTableSelectionChangedEventArgs : EventArgs
+{
+    internal ParametersTableRow Row { get; }
+
+    internal ParametersTableSelectionChangedEventArgs(ParametersTableRow row)
+    {
+        this.Row = row;
+    } // ctor () (ParametersTableRow)
+} // internal sealed class ParametersTableSelectionChangedEventArgs : EventArgs
