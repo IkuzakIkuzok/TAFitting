3748d85da2b15d12e4b1e27b47e79b409a70d38a
ikuzak <k.kohzuki@gmail.com>
2024-09-22 01:34:59 +0900

    Add ModelItem and ModelManager classes for model handling
    Added a sealed class `ModelItem` in `ModelItem.cs` within the
    `TAFitting.Model` namespace. This class has a single property
    `Model` of type `IFittingModel` and a constructor to initialize
    this property.
    
    Added a static class `ModelManager` in `ModelManager.cs` within
    the `TAFitting.Model` namespace. This class manages a collection
    of `IFittingModel` instances identified by GUIDs. It includes
    methods to load models from assemblies, add models to the
    collection, and an event `ModelsChanged` that is triggered when
    models are added. The class also includes a private method
    `TryGetInstance` to create instances of `IFittingModel` types.

---
+++ TAFitting/Model/ModelItem.cs
@@ -0,0 +1,14 @@
+﻿
+// (c) 2024 Kazuki KOHZUKI
+
+namespace TAFitting.Model;
+
+internal sealed class ModelItem
+{
+    internal IFittingModel Model { get; }
+
+    internal ModelItem(IFittingModel model)
+    {
+        this.Model = model;
+    } // internal ModelItem (IFittingModel)
+} // internal sealed class ModelItem
---
+++ TAFitting/Model/ModelManager.cs
@@ -0,0 +1,71 @@
+﻿
+// (c) 2024 Kazuki KOHZUKI
+
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace TAFitting.Model;
+
+internal static class ModelManager
+{
+    private static readonly Dictionary<Guid, IFittingModel> models = [];
+
+    internal static event EventHandler? ModelsChanged;
+
+    internal static IReadOnlyDictionary<Guid, IFittingModel> Models => models;
+
+    static ModelManager()
+    {
+        Load(Assembly.GetExecutingAssembly());
+    } // cctor ()
+
+    internal static void Load(string assemblyPath)
+    {
+        var assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(assemblyPath);
+        Load(assembly);
+    } // internal static void Load (string
+
+    internal static void Load(Assembly assembly)
+    {
+        var added = false;
+
+        var types = assembly.GetTypes();
+        foreach (var type in types)
+        {
+            var guid = type.GUID;
+            if (models.ContainsKey(guid)) continue;
+
+            if (type.IsInterface || type.IsAbstract) continue;
+
+            if (!typeof(IFittingModel).IsAssignableFrom(type)) continue;
+            if (!TryGetInstance(type, out var model)) continue;
+            AddModel(guid, model);
+            added = true;
+        }
+
+        if (added)
+            ModelsChanged?.Invoke(null, EventArgs.Empty);
+    } // internal static void Load (Assembly)
+
+    private static bool TryGetInstance(Type type, [NotNullWhen(true)] out IFittingModel? model)
+    {
+        try
+        {
+            model = Activator.CreateInstance(type) as IFittingModel;
+            return model != null;
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine(e);
+            model = null;
+            return false;
+        }
+    } // private static bool TryGetInstance (Type, out IFittingModel?)
+
+    private static void AddModel(Guid guid, IFittingModel model)
+    {
+        models.Add(guid, model);
+    } // private static void AddModel (Guid, IFittingModel)
+} // internal static class ModelManager
