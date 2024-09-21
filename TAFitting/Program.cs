1782757774d846a680eb862a5fda90bb94cc98b0
ikuzak <k.kohzuki@gmail.com>
2024-09-21 16:38:00 +0900

    Add project files.

---
+++ TAFitting/Program.cs
@@ -0,0 +1,19 @@
+
+// (c) 2024 Kazuki KOHZUKI
+
+namespace TAFitting;
+
+internal static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    private static void Main()
+    {
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+        //Application.Run(new Form1());
+    } // private static void Main ()
+} // internal static class Program
