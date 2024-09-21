03426bb711132369d6df1a55aad3e6d224c43181
ikuzak <k.kohzuki@gmail.com>
2024-09-22 01:29:06 +0900

    Add IterUtils.cs with iteration utility methods
    Added a new file `IterUtils.cs` under the `TAFitting` namespace.
    The file includes:
    - A copyright notice for 2024 Kazuki KOHZUKI.
    - A static class `IterUtils` with utility methods for iteration.
    - A method `Enumerate<T>` that:
      - Takes an `IEnumerable<T>` source and an optional integer offset (default is 0).
      - Throws an `ArgumentNullException` if the source is null.
      - Enumerates the elements of the source, returning a tuple of the index (starting from the offset) and the element.

---
+++ TAFitting/IterUtils.cs
@@ -0,0 +1,27 @@
+﻿
+// (c) 2024 Kazuki KOHZUKI
+
+namespace TAFitting;
+
+/// <summary>
+/// Utility methods for iteration.
+/// </summary>
+internal static class IterUtils
+{
+    /// <summary>
+    /// Enumerates the elements of the source.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements.</typeparam>
+    /// <param name="source">The source.</param>
+    /// <param name="offset">The offset.</param>
+    /// <returns>The enumeration of the elements with their index.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> is <c>null</c>.</exception>
+    internal static IEnumerable<(int index, T element)> Enumerate<T>(this IEnumerable<T> source, int offset = 0)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+
+        var index = offset;
+        foreach (var element in source)
+            yield return (index++, element);
+    } // internal static IEnumerable<(int index, T element)> Enumerate<T> (this IEnumerable<T>, [int])
+} // internal static class IterUtils
