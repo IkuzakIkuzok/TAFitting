ef62065cc954c9a02716f0c4e279b13c3d9326dc
ikuzak <k.kohzuki@gmail.com>
2024-09-22 01:33:45 +0900

    Add FileNameHandler class and regex timeout property
    - Added a detailed comment explaining the functionality of the `FileNameHandler` class, including the format patterns and replacement rules.
    - Created an internal static partial class `FileNameHandler` to handle filename generation based on a given format and basename.
    - Added a property `RegexTimeoutMilliseconds` to set the timeout for regular expressions.
    - Defined a private static readonly `Regex` field `re_basename` initialized by the `BasenamePattern` method.
    - Implemented the `GetFileName` method to generate a filename from a basename and format, applying specified replacements and handling regular expressions with a timeout.
    - Added a `GeneratedRegex` attribute to the `BasenamePattern` method to compile the regex pattern for matching `<BASENAME>` and its variations.

---
+++ TAFitting/Data/FileNameHandler.cs
@@ -0,0 +1,97 @@
+﻿
+// (c) 2024 Kazuki KOHZUKI
+
+using System.Text.RegularExpressions;
+
+namespace TAFitting.Data;
+
+/*
+ * Gets filename based on the parent directory name and the format.
+ *
+ * The format can contain the following patterns:
+ *   - <BASENAME>: The parent directory name.
+ *   - <BASENAME|...>: The parent directory name with replacements.
+ * 
+ * Replacement format:
+ *   - <pattern>/<replacement>: Replace the matched pattern with the replacement.
+ *   - r:<pattern>/<replacement>: Replace the matched pattern with the replacement using regular expression.
+ * 
+ * Example:
+ *   Format: "<BASENAME|ms/000us|r:[um]s/>"
+ *   Basename: "1ms"
+ *   Output: "1000"
+ *   Note that the replacement patters can be chained and applied in order.
+ *   <replacement> can be an empty string.
+ */
+
+/// <summary>
+/// Handles the filename of data files.
+/// </summary>
+internal static partial class FileNameHandler
+{
+    /// <summary>
+    /// Gets or sets the timeout for regular expression.
+    /// </summary>
+    internal static int RegexTimeoutMilliseconds { get; set; } = 500;
+
+    private static readonly Regex re_basename = BasenamePattern();
+
+    /// <summary>
+    /// Gets the filename from the basename and the format.
+    /// </summary>
+    /// <param name="basename">The basename.</param>
+    /// <param name="format">The format.</param>
+    /// <returns>The filename from the basename and the format.</returns>
+    internal static string GetFileName(string basename, string format)
+    {
+        var filename = format;
+        try
+        {
+            var ms = re_basename.Matches(format);
+            foreach (Match m in ms)
+            {
+                var pattern = m.Value;
+                var s_basename = basename;
+
+                if (pattern != "<BASENAME>") // <BASENAME|...>
+                {
+                    var replaces = pattern[10..^1].Split('|');
+                    foreach (var replace in replaces)
+                    {
+                        if (replace.StartsWith("r:"))
+                        {
+                            var kv = replace[2..].Split('/');
+
+                            var timeout = TimeSpan.FromMilliseconds(RegexTimeoutMilliseconds);
+                            try
+                            {
+                                var re = new Regex(kv[0], RegexOptions.None, timeout);
+                                s_basename = re.Replace(s_basename, kv[1]);
+                            }
+                            catch (RegexMatchTimeoutException)
+                            {
+                                s_basename = s_basename.Replace(kv[0], kv[1]);
+                            }
+                        }
+                        else
+                        {
+                            var kv = replace.Split('/');
+                            s_basename = s_basename.Replace(kv[0], kv[1]);
+                        }
+                    }
+                }
+
+                filename = filename.Replace(pattern, s_basename);
+            }
+
+            return filename;
+        }
+        catch
+        {
+            return basename;
+        }
+    } // internal static string GetFileName (string, string)
+
+    [GeneratedRegex(@"<BASENAME(\|[^|/]+/[^|/]*)*>", RegexOptions.Compiled)]
+    private static partial Regex BasenamePattern();
+} // internal static class FileNameHandler
