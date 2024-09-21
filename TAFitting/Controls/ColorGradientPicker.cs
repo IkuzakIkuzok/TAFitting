c830ab0a2c78b3d3cdb73f39fed68f58f80b393f
ikuzak <k.kohzuki@gmail.com>
2024-09-22 01:40:42 +0900

    Add ColorGradientPicker class for color gradient selection
    Introduce a new internal class `ColorGradientPicker` in `ColorGradientPicker.cs` for a Windows Forms application. This class inherits from `Form` and provides a dialog for selecting color gradients.
    
    Key features:
    - Fields for managing color gradient, start and end colors, and UI controls.
    - Constructor to initialize form properties and controls.
    - Overloaded constructor for default colors (red and blue).
    - Overridden `ShowDialog` method to return selected colors.
    - Methods for color selection and UI updates.

---
+++ TAFitting/Controls/ColorGradientPicker.cs
@@ -0,0 +1,134 @@
+﻿
+
+// (c) 2024 Kazuki Kohzuki
+
+using LinearGradientMode = System.Drawing.Drawing2D.LinearGradientMode;
+
+namespace TAFitting.Controls;
+
+/// <summary>
+/// Represents a color gradient picker.
+/// </summary>
+[DesignerCategory("Code")]
+internal class ColorGradientPicker : Form
+{
+    protected readonly ColorGradient colorGradient;
+    private (Color Start, Color End) returnColors;
+
+    protected readonly ColorButton start, end;
+
+    protected readonly Label lb_gradient;
+
+    private readonly Button ok, cancel;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ColorGradientPicker"/> class.
+    /// </summary>
+    /// <param name="startColor">The default start color.</param>
+    /// <param name="endColor">The default end color.</param>
+    internal ColorGradientPicker(Color startColor, Color endColor)
+    {
+        this.Text = "Color gradient";
+        this.Size = this.MinimumSize = this.MaximumSize = new(330, 150);
+        this.MaximizeBox = false;
+
+        this.colorGradient = new(startColor, endColor);
+        this.returnColors = (startColor, endColor);
+
+        this.start = new()
+        {
+            Top = 20,
+            Left = 20,
+            Width = 80,
+            TextAlign = ContentAlignment.MiddleCenter,
+            FlatStyle = FlatStyle.Popup,
+            Parent = this,
+        };
+        this.start.Click += SelectStartColor;
+
+        this.end = new()
+        {
+            Top = 20,
+            Left = 220,
+            Width = 80,
+            TextAlign = ContentAlignment.MiddleCenter,
+            FlatStyle = FlatStyle.Popup,
+            Parent = this,
+        };
+        this.end.Click += SelectEndColor;
+
+        this.lb_gradient = new()
+        {
+            Top = 20,
+            Left = 110,
+            Width = 100,
+            Parent = this,
+        };
+
+        this.ok = new()
+        {
+            Text = "OK",
+            Top = 60,
+            Left = 120,
+            Size = new(80, 30),
+            Parent = this,
+        };
+        this.ok.Click += (sender, e) =>
+        {
+            this.returnColors = (this.colorGradient.StartColor, this.colorGradient.EndColor);
+            Close();
+        };
+
+        this.cancel = new()
+        {
+            Text = "Cancel",
+            Top = 60,
+            Left = 220,
+            Size = new(80, 30),
+            Parent = this,
+        };
+        this.cancel.Click += (sender, e) => Close();
+
+        SetColor();
+    } // ctor (Color, Color)
+
+    internal ColorGradientPicker() : this(Color.Red, Color.Blue) { }
+
+    /// <summary>
+    /// Shows the dialog.
+    /// </summary>
+    /// <returns>The selected start and end colors.</returns>
+    new internal (Color Start, Color End) ShowDialog()
+    {
+        base.ShowDialog();
+        return this.returnColors;
+    } // new internal (Color Start, Color End) ShowDialog ()
+
+    private static Color SelectColor(Color color)
+    {
+        using var cd = new ColorDialog()
+        {
+            Color = color,
+        };
+        return cd.ShowDialog() == DialogResult.OK ? cd.Color : color;
+    } // private static Color SelectColor (Color)
+
+    private void SelectStartColor(object? sender, EventArgs e)
+    {
+        this.colorGradient.StartColor = SelectColor(this.colorGradient.StartColor);
+        SetColor();
+    } // private void SelectStartColor (object?, EventArgs)
+
+    private void SelectEndColor(object? sender, EventArgs e)
+    {
+        this.colorGradient.EndColor = SelectColor(this.colorGradient.EndColor);
+        SetColor();
+    } // private void SelectEndColor (object?, EventArgs)
+
+    protected virtual void SetColor()
+    {
+        this.start.Color = this.colorGradient.StartColor;
+        this.end.Color = this.colorGradient.EndColor;
+        this.colorGradient.FillRectangle(this.lb_gradient, LinearGradientMode.Horizontal);
+    } // protected virtual void SetColor ()
+} // internal class ColorGradientPicker
