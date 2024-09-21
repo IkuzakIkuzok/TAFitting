b5ccedfea2be7552e03e968b4e70d4aa7d2db624
ikuzak <k.kohzuki@gmail.com>
2024-09-22 01:36:11 +0900

    Add new fitting models: EmpiricalPowerLaw and Exponential1Component

---
+++ TAFitting/Model/EmpiricalPowerLaw.cs
@@ -0,0 +1,35 @@
+﻿
+// (c) 2024 Kazuki KOHZUKI
+
+using System.Runtime.InteropServices;
+
+namespace TAFitting.Model;
+
+[Guid("84FCCACE-3DDF-42F5-92BF-7C6BE37B45C7")]
+internal sealed class EmpiricalPowerLaw : IFittingModel
+{
+    private static readonly Parameter[] parameters = [
+        new Parameter { Name = "A0", InitialValue = 1e3 },
+        new Parameter {Name = "a", Constraints = ParameterConstraints.Positive, InitialValue = 1.0 },
+        new Parameter {Name = "Alpha", Constraints = ParameterConstraints.Positive, InitialValue = 0.4 },
+    ];
+
+    /// <inheritdoc/>
+    public string Name => "Empirical Power-Law";
+
+    /// <inheritdoc/>
+    public string Description => "Empirical power-law model";
+
+    public string ExcelFormula => "[A0] / ((1 + [a] * $X) ^ [Alpha])";
+
+    /// <inheritdoc/>
+    public IReadOnlyList<Parameter> Parameters => parameters;
+
+    public Func<double, double> GetFunction(IList<double> parameters)
+    {
+        var a0 = parameters[0];
+        var a = parameters[1];
+        var alpha = parameters[2];
+        return x => a0 / Math.Pow(1 + a * x, alpha);
+    } // public Func<double, double> GetFunction (IList<double> parameters)
+} // internal sealed class EmpiricalPowerLaw : IFittingModel
---
+++ TAFitting/Model/Exponential1Component.cs
@@ -0,0 +1,36 @@
+﻿
+// (c) 2024 Kazuki KOHZUKI
+
+using System.Runtime.InteropServices;
+
+namespace TAFitting.Model;
+
+[Guid("5C8EAF4E-C682-4524-BE0B-B0A1970E461B")]
+internal sealed class Exponential1Component : IFittingModel
+{
+    private static readonly Parameter[] parameters = [
+        new() { Name = "A0", },
+        new() { Name = "A1", InitialValue = 1e3 },
+        new() { Name = "T1", Constraints = ParameterConstraints.Positive, InitialValue = 5.0 },
+    ];
+
+    /// <inheritdoc/>
+    public string Name => "Exp1";
+
+    /// <inheritdoc/>
+    public string Description => "1-Component exponential model";
+
+    /// <inheritdoc/>
+    public string ExcelFormula => "[A0] + [A1] * EXP(-$X / [T1])";
+
+    /// <inheritdoc/>
+    public IReadOnlyList<Parameter> Parameters => parameters;
+
+    public Func<double, double> GetFunction(IList<double> parameters)
+    {
+        var a0 = parameters[0];
+        var a1 = parameters[1];
+        var t1 = parameters[2];
+        return x => a0 + a1 * Math.Exp(-x / t1);
+    } // public Func<double, double> GetFunction (IList<double> parameters)
+} // internal sealed class Exponential1Component
