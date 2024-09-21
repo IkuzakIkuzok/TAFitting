b53cbefb7e1c48a8b081bad07af3e05279e2da79
ikuzak <k.kohzuki@gmail.com>
2024-09-22 01:42:03 +0900

    Add DataGridViewNumericBoxCell and DataGridViewNumericBoxColumn
    Introduce new classes to handle numeric values in DataGridView.
    - DataGridViewNumericBoxCell: Inherits from DataGridViewTextBoxCell.
      - Adds properties for IncrementOrderBias, Maximum, Minimum, and Digit.
      - Implements numeric value handling and formatting.
      - Provides methods for value increment/decrement and validation.
    - DataGridViewNumericBoxColumn: Inherits from DataGridViewColumn.
      - Propagates numeric settings to all cells in the column.
      - Configures CellTemplate and automatic sort mode.

---
+++ TAFitting/Controls/DataGridViewNumericBoxCell.cs
@@ -0,0 +1,140 @@
+﻿
+// (c) 2024 Kazuki KOHZUKI
+
+namespace TAFitting.Controls;
+
+/// <summary>
+/// Displays an editable numeric value in a <see cref="DataGridView"/> control.
+/// </summary>
+internal class DataGridViewNumericBoxCell : DataGridViewTextBoxCell
+{
+    /// <summary>
+    /// Gets or sets the bias of the digit order for incrementing.
+    /// </summary>
+    internal double IncrementOrderBias { get; set; } = -1;
+
+    /// <summary>
+    /// Gets or sets the maximum value of the cell.
+    /// </summary>
+    internal double Maximum { get; set; } = double.MaxValue;
+
+    /// <summary>
+    /// Gets or sets the minimum value of the cell.
+    /// </summary>
+    internal double Minimum { get; set; } = double.MinValue;
+
+    internal int Digit { get; set; } = 6;
+
+    protected double defaultValue = 0.0;
+
+    /// <summary>
+    /// Gets the default value of the cell.
+    /// </summary>
+    internal double DefaultValue
+        => this.OwningColumn?.CellTemplate is DataGridViewNumericBoxCell cell ? cell.defaultValue : this.defaultValue;
+
+    /// <summary>
+    /// Initializes a new instance of the DataGridViewNumericBoxCell class.
+    /// </summary>
+    public DataGridViewNumericBoxCell()
+    {
+        this.Style.Format = "N2";
+    } // ctor ()
+
+    /// <summary>
+    /// Initializes a new instance of the DataGridViewNumericBoxCell class
+    /// with the specified default value.
+    /// </summary>
+    /// <param name="defaultValue">The default value of the cell.</param>
+    public DataGridViewNumericBoxCell(double defaultValue) : this()
+    {
+        this.defaultValue = defaultValue;
+    } // ctor (double)
+
+    override public Type ValueType => typeof(double);
+
+    override public object DefaultNewRowValue => this.DefaultValue;
+
+    /// <inheritdoc/>
+    override protected object GetFormattedValue(object value, int rowIndex, ref DataGridViewCellStyle cellStyle, TypeConverter valueTypeConverter, TypeConverter formattedValueTypeConverter, DataGridViewDataErrorContexts context)
+    {
+        if (value is double d)
+            return d.ToString("N2");
+        return base.GetFormattedValue(value, rowIndex, ref cellStyle, valueTypeConverter, formattedValueTypeConverter, context);
+    } // override protected object GetFormattedValue (object, int, ref DataGridViewCellStyle, TypeConverter, TypeConverter, DataGridViewDataErrorContexts)
+
+    override protected bool SetValue(int rowIndex, object value)
+    {
+        if (value is string s && double.TryParse(s, out var d))
+        {
+            d = Math.Max(this.Minimum, Math.Min(this.Maximum, d));
+            return base.SetValue(rowIndex, d);
+        }
+        return base.SetValue(rowIndex, value);
+    } // override protected bool SetValue (int, object)
+
+    protected double GetDoubleValue(int rowIndex)
+        => Math.Round((double)GetValue(rowIndex), this.Digit);
+
+    override protected void OnKeyDown(KeyEventArgs e, int rowIndex)
+    {
+        if (e.Alt && this.DataGridView is not null)
+        {
+            var additionalBias = e.Shift ? 10 : 1;
+
+            if (e.KeyCode == Keys.Up)
+            {
+                var value = GetDoubleValue(rowIndex);
+                var increment = CalcIncrement() * additionalBias;
+                if (increment == 0) increment = 1;
+                SetValue(rowIndex, Math.Floor(Math.Round(value / increment, this.Digit)) * increment + increment);
+                e.Handled = true;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                var value = GetDoubleValue(rowIndex);
+                var decrement = CalcDecrement() * additionalBias;
+                if (decrement == 0) decrement = 1;
+                var newValue = Math.Ceiling(Math.Round(value / decrement, this.Digit)) * decrement - decrement;
+                SetValue(rowIndex, newValue);
+                e.Handled = true;
+            }
+        }
+
+        if (!e.Handled)
+            base.OnKeyDown(e, rowIndex);
+    } // override protected void OnKeyDown (KeyEventArgs, int)
+
+    /// <summary>
+    /// Gets the logarithmic value of the cell.
+    /// </summary>
+    /// <returns>The logarithmic value.</returns>
+    protected virtual double GetLogValue()
+    {
+        var val = (double)GetValue(this.RowIndex);
+        if (val <= 0)
+            val = this.DefaultValue;
+        return Math.Log10(val);
+    } // protected virtual double GetLogValue ()
+
+    /// <summary>
+    /// Calculates the increment value.
+    /// </summary>
+    /// <returns>The increment value.</returns>
+    protected virtual double CalcIncrement()
+    {
+        var order = Math.Floor(GetLogValue()) + this.IncrementOrderBias;
+        return Math.Pow(10, order);
+    } // protected virtual double CalcIncrement ()
+
+    /// <summary>
+    /// Calculates the decrement value.
+    /// </summary>
+    /// <returns>The decrement value.</returns>
+    protected virtual double CalcDecrement()
+    {
+        var log = GetLogValue();
+        var order = Math.Floor(log) + this.IncrementOrderBias;
+        return log % 1 == 0 ? Math.Pow(10, order - 1) : Math.Pow(10, order);
+    } // protected virtual double CalcDecrement ()
+} // internal class DataGridViewNumericBoxCell : DataGridViewTextBoxCell
---
+++ TAFitting/Controls/DataGridViewNumericBoxColumn.cs
@@ -0,0 +1,90 @@
+﻿
+// (c) 2024 Kazuki KOHZUKI
+
+namespace TAFitting.Controls;
+
+
+/// <summary>
+/// Represents a column that contains <see cref="DataGridViewNumericBoxCell"/> objects.
+/// </summary>
+internal class DataGridViewNumericBoxColumn : DataGridViewColumn
+{
+    /// <summary>
+    /// Gets or sets the bias of the digit order for incrementing.
+    /// </summary>
+    internal double IncrementOrderBias
+    {
+        get => ((DataGridViewNumericBoxCell)this.CellTemplate).IncrementOrderBias;
+        set
+        {
+            ((DataGridViewNumericBoxCell)this.CellTemplate).IncrementOrderBias = value;
+            var dgw = this.DataGridView;
+            if (dgw is not null)
+            {
+                for (var i = 0; i < dgw.RowCount; i++)
+                {
+                    var cell = (DataGridViewNumericBoxCell)dgw[this.Index, i];
+                    cell.IncrementOrderBias = value;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the maximum value of the cells in the column.
+    /// </summary>
+    internal double Maximum
+    {
+        get => ((DataGridViewNumericBoxCell)this.CellTemplate).Maximum;
+        set
+        {
+            ((DataGridViewNumericBoxCell)this.CellTemplate).Maximum = value;
+            var dgw = this.DataGridView;
+            if (dgw is not null)
+            {
+                for (var i = 0; i < dgw.RowCount; i++)
+                {
+                    var cell = (DataGridViewNumericBoxCell)dgw[this.Index, i];
+                    cell.Maximum = value;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the minimum value of the cells in the column.
+    /// </summary>
+    internal double Minimum
+    {
+        get => ((DataGridViewNumericBoxCell)this.CellTemplate).Minimum;
+        set
+        {
+            ((DataGridViewNumericBoxCell)this.CellTemplate).Minimum = value;
+            var dgw = this.DataGridView;
+            if (dgw is not null)
+            {
+                for (var i = 0; i < dgw.RowCount; i++)
+                {
+                    var cell = (DataGridViewNumericBoxCell)dgw[this.Index, i];
+                    cell.Minimum = value;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DataGridViewNumericBoxColumn"/> class.
+    /// </summary>
+    internal DataGridViewNumericBoxColumn() : this(0.0) { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DataGridViewNumericBoxColumn"/> class
+    /// with the specified default value.
+    /// </summary>
+    /// <param name="defaultValue">The default value.</param>
+    internal DataGridViewNumericBoxColumn(double defaultValue)
+    {
+        this.CellTemplate = new DataGridViewNumericBoxCell(defaultValue);
+        this.SortMode = DataGridViewColumnSortMode.Automatic;
+    } // ctor (double)
+} // internal class DataGridViewNumericBoxColumn : DataGridViewColumn
