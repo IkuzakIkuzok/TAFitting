facb44cdb10f88ba244e555e1dcb96b4eef5d393
Kazuki KOHZUKI <k.kohzuki@gmail.com>
2024-10-02 18:13:12 +0900

    Add new models and update project references
    - Introduced new namespaces `TAFitting.Model.Exponential` and `TAFitting.Model.Polynomial`.
    - Added new classes `Exponential1`, `Exponential2`, `Exponential3`, and `Exponential4` in `ExponentialModels.cs`, each with an `ExponentialModel` attribute and a `Guid` attribute, and a `Name` property returning a string.
    - Added new classes `Polynomial1` through `Polynomial9` in `PolynomialModels.cs`, each with a `PolynomialModel` attribute and a `Guid` attribute, and a `Name` property returning a string.
    - Updated `TAFitting.csproj` to include a new package reference for `Microsoft.Net.Compilers.Toolset` version `4.11.0` with specific asset inclusion settings.
    - Modified the `ProjectReference` for `TAFitting.ModelGenerator` in `TAFitting.csproj` to be on a single line.
    - Added `System.Runtime.InteropServices` to the `Using` directives in `TAFitting.csproj`.

---
+++ TAFitting/Model/PolynomialModels.cs
@@ -0,0 +1,67 @@
+﻿
+// (c) 2024 Kazuki KOHZUKI
+
+namespace TAFitting.Model.Polynomial;
+
+[PolynomialModel(1)]
+[Guid("9C488607-39F4-48EE-ADDF-E15CB94FF86F")]
+internal sealed partial class Polynomial1
+{
+    public string Name => "Poly1";
+} // internal sealed partial class Polynomial1
+
+[PolynomialModel(2)]
+[Guid("99C057E2-D53E-4110-9610-FEF403D75527")]
+internal sealed partial class Polynomial2
+{
+    public string Name => "Poly2";
+} // internal sealed partial class Polynomial2
+
+[PolynomialModel(3)]
+[Guid("88F0E853-73C6-450B-B5BE-F37B26196D37")]
+internal sealed partial class Polynomial3
+{
+    public string Name => "Poly3";
+} // internal sealed partial class Polynomial3
+
+[PolynomialModel(4)]
+[Guid("03980EB3-7963-4EC3-91D8-A436DAECE02D")]
+internal sealed partial class Polynomial4
+{
+    public string Name => "Poly4";
+} // internal sealed partial class Polynomial4
+
+[PolynomialModel(5)]
+[Guid("C7773F3D-44D1-48B0-BF58-746B58433CC0")]
+internal sealed partial class Polynomial5
+{
+    public string Name => "Poly5";
+} // internal sealed partial class Polynomial5
+
+[PolynomialModel(6)]
+[Guid("82F9F05F-91FE-46B4-9CC2-3F792DF5DB83")]
+internal sealed partial class Polynomial6
+{
+    public string Name => "Poly6";
+} // internal sealed partial class Polynomial6
+
+[PolynomialModel(7)]
+[Guid("6E27A0B6-51BF-4D65-918F-CC4B0FDADF60")]
+internal sealed partial class Polynomial7
+{
+    public string Name => "Poly7";
+} // internal sealed partial class Polynomial7
+
+[PolynomialModel(8)]
+[Guid("F68E43C2-6417-480C-BEB4-898B7F335757")]
+internal sealed partial class Polynomial8
+{
+    public string Name => "Poly8";
+} // internal sealed partial class Polynomial8
+
+[PolynomialModel(9)]
+[Guid("913359D9-FA8A-43FF-AEFD-4C2E48DD5A28")]
+internal sealed partial class Polynomial9
+{
+    public string Name => "Poly9";
+} // internal sealed partial class Polynomial9
