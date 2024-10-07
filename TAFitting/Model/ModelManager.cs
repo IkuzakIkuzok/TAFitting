2f63a09d88a7be00d643946c46b94149310cac85
ikuzak <k.kohzuki@gmail.com>
2024-10-07 16:15:48 +0900

    Added XML documentation comments to `ModelManager` class and methods.

--- TAFitting/Model/ModelManager.cs
+++ TAFitting/Model/ModelManager.cs
@@ -8,15 +8,27 @@ using System.Runtime.Loader;
 
 namespace TAFitting.Model;
 
+/// <summary>
+/// Manages the fitting models.
+/// </summary>
 internal static class ModelManager
 {
     private static readonly Dictionary<Guid, ModelItem> models = [];
     private static readonly Dictionary<Guid, List<IEstimateProvider>> estimateProviders = [];
 
+    /// <summary>
+    /// Occurs when the models are changed.
+    /// </summary>
     internal static event EventHandler? ModelsChanged;
 
+    /// <summary>
+    /// Gets the models.
+    /// </summary>
     internal static IReadOnlyDictionary<Guid, ModelItem> Models => models;
 
+    /// <summary>
+    /// Gets the estimate providers.
+    /// </summary>
     internal static IReadOnlyDictionary<Guid, List<IEstimateProvider>> EstimateProviders => estimateProviders;
 
     static ModelManager()
@@ -41,12 +53,20 @@ internal static class ModelManager
         }
     } // cctor ()
 
+    /// <summary>
+    /// Loads models from the specified assembly.
+    /// </summary>
+    /// <param name="assemblyPath">The path to the assembly.</param>
     internal static void Load(string assemblyPath)
     {
         var assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(assemblyPath);
         Load(assembly);
     } // internal static void Load (string
 
+    /// <summary>
+    /// Loads models from the specified assembly.
+    /// </summary>
+    /// <param name="assembly">The assembly.</param>
     internal static void Load(Assembly assembly)
     {
         var types = assembly.GetTypes();
@@ -54,6 +74,10 @@ internal static class ModelManager
             AddType(type);
     } // internal static void Load (Assembly)
 
+    /// <summary>
+    /// Add the specified type to the models if it is a fitting model or an estimate provider.
+    /// </summary>
+    /// <param name="type">The type.</param>
     private static void AddType(Type type)
     {
         var guid = type.GUID;
@@ -78,6 +102,12 @@ internal static class ModelManager
         }
     } // private static void AddType (Type)
 
+    /// <summary>
+    /// Tries to create an instance of the specified type of the model.
+    /// </summary>
+    /// <param name="type">The type of the model to create an instance.</param>
+    /// <param name="model">When this method returns, contains the model instance, if the model is created successfully; otherwise, <see langword="null"/>.</param>
+    /// <returns><see langword="true"/> if the model is created successfully; otherwise, <see langword="false"/>.</returns>
     private static bool TryGetModelInstance(Type type, [NotNullWhen(true)] out IFittingModel? model)
     {
         try
@@ -93,6 +123,12 @@ internal static class ModelManager
         }
     } // private static bool TryGetModelInstance (Type, out IFittingModel?)
 
+    /// <summary>
+    /// Triest to create an instance of the specified type of the estimate provider.
+    /// </summary>
+    /// <param name="type">The type of the estimate provider to create an instance.</param>
+    /// <param name="provider">When this method returns, contains the estimate provider instance, if the provider is created successfully; otherwise, <see langword="null"/>.</param>
+    /// <returns><see langword="true"/> if the estimate provider is created successfully; otherwise, <see langword="false"/>.</returns>
     private static bool TryGetEstimateProviderInstance(Type type, [NotNullWhen(true)] out IEstimateProvider? provider)
     {
         try
@@ -114,6 +150,12 @@ internal static class ModelManager
         AddModel(guid, model, category);
     } // private static void AddModel (Guid, IFittingModel)
 
+    /// <summary>
+    /// Adds the specified model to the models.
+    /// </summary>
+    /// <param name="guid">The GUID of the model.</param>
+    /// <param name="model">The model.</param>
+    /// <param name="category">The category of the model.</param>
     internal static void AddModel(Guid guid, IFittingModel model, string category)
     {
         var item = new ModelItem(model, category);
@@ -121,6 +163,10 @@ internal static class ModelManager
         ModelsChanged?.Invoke(null, EventArgs.Empty);
     } // internal static void AddModel (Guid, IFittingModel, string)
 
+    /// <summary>
+    /// Adds the specified estimate provider.
+    /// </summary>
+    /// <param name="provider">The estimate provider.</param>
     private static void AddEstimateProvider(IEstimateProvider provider)
     {
         foreach (var supported in provider.SupportedModels)
@@ -132,6 +178,10 @@ internal static class ModelManager
         ModelsChanged?.Invoke(null, EventArgs.Empty);
     } // private static void AddEstimateProvider (IEstimateProvider)
 
+    /// <summary>
+    /// Removes the model with the specified GUID.
+    /// </summary>
+    /// <param name="guid">The GUID of the model.</param>
     internal static void RemoveModel(Guid guid)
     {
         models.Remove(guid);
