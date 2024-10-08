48ab19a3725d2a3cd705b108a351c0c17266bad6
Kazuki KOHZUKI <k.kohzuki@gmail.com>
2024-10-08 09:20:21 +0900

    Refactor model attributes to include Name property
    Updated `ExponentialModel` and `PolynomialModel` attributes in `ExponentialModels.cs` and `PolynomialModels.cs` to include a `Name` property. Removed the `Name` property from each class and streamlined class declarations to `internal sealed partial class`.

--- TAFitting/Model/ExponentialModels.cs
+++ TAFitting/Model/ExponentialModels.cs
@@ -3,30 +3,18 @@
 
 namespace TAFitting.Model.Exponential;
 
-[ExponentialModel(1)]
+[ExponentialModel(1, Name = "Exp1")]
 [Guid("5C8EAF4E-C682-4524-BE0B-B0A1970E461B")]
-internal sealed partial class Exponential1
-{
-    public string Name => "Exp1";
-} // internal sealed partial class Exponential1
+internal sealed partial class Exponential1;
 
-[ExponentialModel(2)]
+[ExponentialModel(2, Name = "Exp2")]
 [Guid("08A2DDE9-FB2E-49FF-AC53-D78275CE7022")]
-internal sealed partial class Exponential2
-{
-    public string Name => "Exp2";
-} // internal sealed partial class Exponential2
+internal sealed partial class Exponential2;
 
-[ExponentialModel(3)]
+[ExponentialModel(3, Name = "Exp3")]
 [Guid("AD397D83-B21F-4861-BFF9-0AEC16FE0AF8")]
-internal sealed partial class Exponential3
-{
-    public string Name => "Exp3";
-} // internal sealed partial class Exponential3
+internal sealed partial class Exponential3;
 
-[ExponentialModel(4)]
+[ExponentialModel(4, Name = "Exp4")]
 [Guid("E02DAD2A-9DC9-4315-AACA-F62DEB9681A7")]
-internal sealed partial class Exponential4
-{
-    public string Name => "Exp4";
-} // internal sealed partial class Exponential4
+internal sealed partial class Exponential4;
--- TAFitting/Model/PolynomialModels.cs
+++ TAFitting/Model/PolynomialModels.cs
@@ -3,65 +3,38 @@
 
 namespace TAFitting.Model.Polynomial;
 
-[PolynomialModel(1)]
+[PolynomialModel(1, Name = "Poly1")]
 [Guid("9C488607-39F4-48EE-ADDF-E15CB94FF86F")]
-internal sealed partial class Polynomial1
-{
-    public string Name => "Poly1";
-} // internal sealed partial class Polynomial1
+internal sealed partial class Polynomial1;
 
-[PolynomialModel(2)]
+[PolynomialModel(2, Name = "Poly2")]
 [Guid("99C057E2-D53E-4110-9610-FEF403D75527")]
-internal sealed partial class Polynomial2
-{
-    public string Name => "Poly2";
-} // internal sealed partial class Polynomial2
+internal sealed partial class Polynomial2;
 
-[PolynomialModel(3)]
+[PolynomialModel(3, Name = "Poly3")]
 [Guid("88F0E853-73C6-450B-B5BE-F37B26196D37")]
-internal sealed partial class Polynomial3
-{
-    public string Name => "Poly3";
-} // internal sealed partial class Polynomial3
+internal sealed partial class Polynomial3;
 
-[PolynomialModel(4)]
+[PolynomialModel(4, Name = "Poly4")]
 [Guid("03980EB3-7963-4EC3-91D8-A436DAECE02D")]
-internal sealed partial class Polynomial4
-{
-    public string Name => "Poly4";
-} // internal sealed partial class Polynomial4
+internal sealed partial class Polynomial4;
 
-[PolynomialModel(5)]
+[PolynomialModel(5, Name = "Poly5")]
 [Guid("C7773F3D-44D1-48B0-BF58-746B58433CC0")]
-internal sealed partial class Polynomial5
-{
-    public string Name => "Poly5";
-} // internal sealed partial class Polynomial5
+internal sealed partial class Polynomial5;
 
-[PolynomialModel(6)]
+[PolynomialModel(6, Name = "Poly6")]
 [Guid("82F9F05F-91FE-46B4-9CC2-3F792DF5DB83")]
-internal sealed partial class Polynomial6
-{
-    public string Name => "Poly6";
-} // internal sealed partial class Polynomial6
+internal sealed partial class Polynomial6;
 
-[PolynomialModel(7)]
+[PolynomialModel(7, Name = "Poly7")]
 [Guid("6E27A0B6-51BF-4D65-918F-CC4B0FDADF60")]
-internal sealed partial class Polynomial7
-{
-    public string Name => "Poly7";
-} // internal sealed partial class Polynomial7
+internal sealed partial class Polynomial7;
 
-[PolynomialModel(8)]
+[PolynomialModel(8, Name = "Poly8")]
 [Guid("F68E43C2-6417-480C-BEB4-898B7F335757")]
-internal sealed partial class Polynomial8
-{
-    public string Name => "Poly8";
-} // internal sealed partial class Polynomial8
+internal sealed partial class Polynomial8;
 
-[PolynomialModel(9)]
+[PolynomialModel(9, Name = "Poly9")]
 [Guid("913359D9-FA8A-43FF-AEFD-4C2E48DD5A28")]
-internal sealed partial class Polynomial9
-{
-    public string Name => "Poly9";
-} // internal sealed partial class Polynomial9
+internal sealed partial class Polynomial9;
