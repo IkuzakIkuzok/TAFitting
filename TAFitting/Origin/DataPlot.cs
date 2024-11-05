45d796cb53b2b4329c6b880565aee8ac668263a1
Kazuki KOHZUKI <k.kohzuki@gmail.com>
2024-11-05 12:53:12 +0900

    Add Name and LongName properties to DataPlot class
    Introduced two new properties, `Name` and `LongName`, to the `DataPlot` class. These properties allow getting and setting the name and long name of the data plot, respectively. Both properties have `internal` access modifiers, making them accessible within the same assembly.

--- TAFitting/Origin/DataPlot.cs
+++ TAFitting/Origin/DataPlot.cs
@@ -11,6 +11,24 @@ internal class DataPlot
     private readonly dynamic dataPlot;
 
     /// <summary>
+    /// Gets or sets the name of the data plot.
+    /// </summary>
+    internal string Name
+    {
+        get => this.dataPlot.Name;
+        set => this.dataPlot.Name = value;
+    }
+
+    /// <summary>
+    /// Gets or sets the long name of the data plot.
+    /// </summary>
+    internal string LongName
+    {
+        get => this.dataPlot.LongName;
+        set => this.dataPlot.LongName = value;
+    }
+
+    /// <summary>
     /// Initializes a new instance of the <see cref="DataPlot"/> class with the specified data plot.
     /// </summary>
     /// <param name="dataPlot">The Origin data plot object.</param>
