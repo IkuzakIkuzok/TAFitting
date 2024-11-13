03c2e77e68b921995e39154267b9f4116e41099c
Kazuki KOHZUKI <k.kohzuki@gmail.com>
2024-11-13 10:53:28 +0900

    Add ControlDrawingSuspender class to manage control drawing
    Introduce ControlDrawingSuspender class in ControlDrawingSuspender.cs
    to suspend and resume control drawing for performance improvements.
    Implement IDisposable to ensure drawing resumes on disposal. Add
    StopPainting and ResumePainting methods to control drawing state.
    
    Update Win32.cs with Win32 static class for API functions and constants.
    Include WM_SETREDRAW constant and SendMessage function from user32.dll
    to manage control drawing within ControlDrawingSuspender.

---
+++ TAFitting/Controls/ControlDrawingSuspender.cs
@@ -0,0 +1,62 @@
+﻿
+// (c) 2024 Kazuki KOHZUKI
+
+using static TAFitting.Win32;
+
+namespace TAFitting.Controls;
+
+/// <summary>
+/// Suspends drawing of the specified control.
+/// </summary>
+internal sealed class ControlDrawingSuspender : IDisposable
+{
+    private readonly Control _control;
+    private bool _disposed = false;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ControlDrawingSuspender"/> class.
+    /// </summary>
+    /// <param name="control">The control.</param>
+    internal ControlDrawingSuspender(Control control)
+    {
+        this._control = control;
+        StopPainting(control);
+    } // ctor (Control)
+
+    /// <summary>
+    /// Stops painting of the specified control.
+    /// </summary>
+    /// <param name="control">The control.</param>
+    internal static void StopPainting(Control control)
+    {
+        SendMessage(new HandleRef(control, control.Handle), WM_SETREDRAW, IntPtr.Zero, IntPtr.Zero);
+    } // internal static void StopPainting (Control)
+
+    /// <summary>
+    /// Resumes painting of the specified control.
+    /// </summary>
+    /// <param name="control">The control.</param>
+    internal static void ResumePainting(Control control)
+    {
+        SendMessage(new HandleRef(control, control.Handle), WM_SETREDRAW, 1, IntPtr.Zero);
+        control.Refresh();
+    } // internal static void ResumePainting (Control)
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    } // public void Dispose ()
+
+    private void Dispose(bool disposing)
+    {
+        if (!this._disposed)
+        {
+            if (disposing)
+            {
+                ResumePainting(this._control);
+            }
+            this._disposed = true;
+        }
+    } // private void Dispose (bool)
+} // internal sealed class ControlDrawingSuspender : IDisposable
---
+++ TAFitting/Win32.cs
@@ -0,0 +1,29 @@
+﻿
+// (c) 2024 Kazuki KOHZUKI
+
+namespace TAFitting;
+
+/// <summary>
+/// Provides Win32 API functions and constants.
+/// </summary>
+internal static class Win32
+{
+    internal const int WM_SETREDRAW = 0x000B;
+    internal const int HWND_BROADCAST = 0xffff;
+
+    /// <summary>
+    /// Sends the specified message to a window or windows.
+    /// The <see cref="SendMessage"/> function calls the window procedure for the specified window
+    /// and does not return until the window procedure has processed the message.
+    /// </summary>
+    /// <param name="hWnd">A handle to the window whose window procedure will receive the message.
+    /// If this parameter is <see cref="HWND_BROADCAST"/> ((HWND)0xffff), the message is sent to all top-level windows in the system,
+    /// including disabled or invisible unowned windows, overlapped windows, and pop-up windows;
+    /// but the message is not sent to child windows.</param>
+    /// <param name="msg">The message to be sent.</param>
+    /// <param name="wParam">Additional message-specific information.</param>
+    /// <param name="lParam">Additional message-specific information.</param>
+    /// <returns>The return value specifies the result of the message processing; it depends on the message sent.</returns>
+    [DllImport("user32.dll")]
+    internal static extern IntPtr SendMessage(HandleRef hWnd, int msg, IntPtr wParam, IntPtr lParam);
+} // internal static class Win32
