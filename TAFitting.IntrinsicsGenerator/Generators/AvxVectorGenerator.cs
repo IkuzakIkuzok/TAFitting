07e8b83c57c6a34aaa9a209b6e5982aee77a8361
Kazuki KOHZUKI <k.kohzuki@gmail.com>
2024-11-27 16:40:56 +0900

    Rename 'table' to 'exp_table' in AvxVectorGenerator.cs
    Renamed the static readonly array 'table' to 'exp_table' to better reflect its purpose. Updated the declaration, fixed statement, and variable assignment to use 'exp_table' instead of 'table'.

--- TAFitting.IntrinsicsGenerator/Generators/AvxVectorGenerator.cs
+++ TAFitting.IntrinsicsGenerator/Generators/AvxVectorGenerator.cs
@@ -87,7 +87,7 @@ namespace TAFitting.Data
         private static readonly Vector256<ulong> Mask11;
         private static readonly Vector256<ulong> Adj;
 
-        private static readonly ulong[] table = [
+        private static readonly ulong[] exp_table = [
             {string.Join(",\n\t\t\t", GenerateElements(s, 8, i => ComputeTable(i).ToString() + "UL", ", "))}
         ];
 
@@ -143,7 +143,7 @@ namespace TAFitting.Data
                 y = Avx.Add(Avx.Subtract(y, t), C1);
 
                 var adr = Avx2.And(i, Mask11).AsInt64();
-                fixed (ulong* p = table)
+                fixed (ulong* p = exp_table)
                 {{
                     var iax = Avx2.GatherVector256(p, adr, 8);
                     i = Avx2.Or(u, iax);
@@ -174,7 +174,7 @@ namespace TAFitting.Data
 
             var d = x * ({2048 / Math.Log(2)}) + ({3UL << 51});
             var i = BitConverter.DoubleToUInt64Bits(d);
-            var iax = table[i & 2047];
+            var iax = exp_table[i & 2047];
             var t = (d - ({3UL << 51})) * {Math.Log(2) / 2048} - x;
             var u = ((i + 2095104) >> 11) << 52;
             var y = (3.0000000027955394 - t) * (t * t) * 0.16666666685227835 - t + 1;
