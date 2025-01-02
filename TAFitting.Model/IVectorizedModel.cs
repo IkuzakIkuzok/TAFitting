c1dfcf6859a411a3b977f34d816116d12f90877f
Kazuki KOHZUKI <k.kohzuki@gmail.com>
2025-01-02 21:41:21 +0900

    Add remarks to clarify GetVectorizedDerivatives behavior
    Added a <remarks> section to the XML documentation of the
    GetVectorizedDerivatives method in the IVectorizedModel<TVector>
    interface. The remarks explain that the second argument of the
    returned Action<TVector, TVector[]>, which will contain the
    derivatives at the end of the method, is not initialized to 0
    when passed to the method. It suggests calling TVector.Load(double)
    to fill the array with 0 if necessary.

--- TAFitting.Model/IVectorizedModel.cs
+++ TAFitting.Model/IVectorizedModel.cs
@@ -23,5 +23,10 @@ public interface IVectorizedModel<TVector> : IFittingModel where TVector : IIntr
     /// </summary>
     /// <param name="parameters">The parameters of the model.</param>
     /// <returns>The vectorized derivatives of the model with respect to the parameters.</returns>
+    /// <remarks>
+    /// The 2nd argument of the returned <see cref="Action{TVector, TVector[]}"/>, which will contain the derivatives at the end of the method,
+    /// is not filled with <c>0</c> when it is passed to the method.
+    /// Therefore, call <see cref="TVector.Load(double)"/> to fill the array with <c>0</c> if necessary.
+    /// </remarks>
     public Action<TVector, TVector[]> GetVectorizedDerivatives(IReadOnlyList<double> parameters);
 } // public interface IVectorizedModel<TVector> : IFittingModel where TVector : IIntrinsicVector<TVector>
