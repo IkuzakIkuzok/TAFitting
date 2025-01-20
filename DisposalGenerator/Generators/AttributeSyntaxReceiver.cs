dcbed6aa6cb74c03faa0c3472f9e2be8a0f708b5
Kazuki KOHZUKI <k.kohzuki@gmail.com>
2025-01-20 15:25:08 +0900

    Add DisposalGenerator project with source generators
    A new project named `DisposalGenerator` has been added to the solution, targeting `netstandard2.0`. This project includes several properties such as `LangVersion`, `Nullable`, `ImplicitUsings`, `Configurations`, and `Platforms`.
    
    The `DisposalGenerator` project references several NuGet packages, including `Microsoft.CodeAnalysis.CSharp.Workspaces`, `Microsoft.CodeAnalysis.CSharp`, and `Microsoft.CodeAnalysis.Analyzers`. It also includes a project reference to `TAFitting.SourceGeneratorUtils`.
    
    Several `using` directives have been added to the project file for namespaces like `Microsoft.CodeAnalysis`, `System.Text`, `Microsoft.CodeAnalysis.CSharp`, and `Microsoft.CodeAnalysis.CSharp.Syntax`.
    
    New classes `AttributeDeclarationItem<T>`, `AttributeSyntaxReceiver`, `AttributesGenerator`, and `AutoDisposalGenerator` have been added. These classes implement functionality for generating attributes and disposal logic for classes marked with specific attributes.
    
    The solution file `TAFitting.sln` has been updated to include the new `DisposalGenerator` project and its configurations for different build platforms (Any CPU, x64, x86) and build configurations (Debug, Release).

---
+++ DisposalGenerator/Generators/AttributeSyntaxReceiver.cs
@@ -0,0 +1,38 @@
+﻿
+// (c) 2024-2025 Kazuki Kohzuki
+
+namespace DisposalGenerator.Generators;
+
+internal sealed class AttributeSyntaxReceiver : ISyntaxReceiver
+{
+    private readonly List<AttributeDeclarationItem<ClassDeclarationSyntax>> classes = [];
+
+    internal IReadOnlyCollection<string> AttributeName { get; }
+
+    internal IReadOnlyList<AttributeDeclarationItem<ClassDeclarationSyntax>> Classes => this.classes;
+
+    internal AttributeSyntaxReceiver(string attributeName)
+    {
+        var arr = new string[2];
+
+        var className = attributeName.Split('.').Last();
+        arr[0] = className;
+        if (className.EndsWith("Attribute"))
+            arr[1] = className[..^9];
+        else
+            arr[1] = className + "Attribute";
+
+        this.AttributeName = arr;
+    } // internal AttributeSyntaxReceiver (string)
+
+    public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+    {
+        if (syntaxNode is not ClassDeclarationSyntax classDeclarationSyntax) return;
+        if (classDeclarationSyntax.AttributeLists.Count == 0) return;
+
+        var attrs = classDeclarationSyntax.AttributeLists.SelectMany(al => al.Attributes);
+        var attr = attrs.FirstOrDefault(a => this.AttributeName.Contains(a.Name.ToFullString().Split('.').Last()));
+        if (attr is null) return;
+        this.classes.Add(new(classDeclarationSyntax, attr));
+    } // public void OnVisitSyntaxNode (SyntaxNode)
+} // internal sealed class AttributeSyntaxReceiver : ISyntaxReceiver
