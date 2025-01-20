4f18431f374702fddb365439a04e9ac3dfde6a29
Kazuki KOHZUKI <k.kohzuki@gmail.com>
2025-01-20 16:00:29 +0900

    Add analyzers and code fixes for attribute usage
    Introduce AttributeUsageAnalyzer to ensure classes with specific attributes are marked as partial and not static, reporting diagnostics DG0001 and DG0002. Add AddPartialCodeFixProvider to address DG0001 by adding the partial modifier, and RemoveStaticCodeFixProvider to address DG0002 by removing the static modifier.

---
+++ DisposalGenerator/Analyzers/AttributeUsageAnalyzer.cs
@@ -0,0 +1,81 @@
+﻿
+// (c) 2025 Kazuki Kohzuki
+
+using DisposalGenerator.Generators;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using TAFitting.SourceGeneratorUtils;
+
+namespace DisposalGenerator.Analyzers;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+internal sealed class AttributeUsageAnalyzer: DiagnosticAnalyzer
+{
+    internal const string PartialErrId = "DG0001";
+    internal const string StaticErrId  = "DG0002";
+
+#pragma warning disable IDE0079
+#pragma warning disable RS2008
+
+    private static readonly DiagnosticDescriptor PartialErr = new(
+        id                : PartialErrId,
+        title             : "Missing partial modifier",
+        messageFormat     : "The class with '{0}' must be partial",
+        category          : "Usage",
+        defaultSeverity   : DiagnosticSeverity.Error,
+        isEnabledByDefault: true
+    );
+
+    private static readonly DiagnosticDescriptor StaticErr = new(
+        id                : StaticErrId,
+        title             : "Static modifier",
+        messageFormat     : "The class with '{0}' must not be static",
+        category          : "Usage",
+        defaultSeverity   : DiagnosticSeverity.Error,
+        isEnabledByDefault: true
+    );
+
+    private static readonly string[] attributes;
+
+    internal static IReadOnlyCollection<string> Attributes => attributes;
+
+    static AttributeUsageAnalyzer()
+    {
+        attributes = [
+            AttributesGenerator.AutoDisposalAttributeName,
+        ];
+    } // cctor ()
+
+    override public ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        => [PartialErr, StaticErr];
+
+    override public void Initialize(AnalysisContext context)
+    {
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution();
+
+        context.RegisterSyntaxNodeAction(AnalyzeClass, SyntaxKind.ClassDeclaration);
+    } // public void Initialize (AnalysisContext)
+
+    private static void AnalyzeClass(SyntaxNodeAnalysisContext context)
+    {
+        var syntax = (ClassDeclarationSyntax)context.Node;
+        if (syntax.AttributeLists.Count == 0) return;
+
+        var attrs = syntax.AttributeLists.SelectMany(al => al.Attributes);
+        var vectorAttrs = attrs.Where(a => attributes.Contains(a.GetGetFullyQualifiedName(context))).ToArray();
+        if (vectorAttrs.Length == 0) return;
+
+        var attr = vectorAttrs[0];
+        var attrName = attr.GetGetFullyQualifiedName(context).Split('.').Last();
+
+        var modifiers = syntax.Modifiers.Select(m => m.Text);
+        var isPartial = modifiers.Contains("partial");
+        if (!isPartial)
+            context.ReportDiagnostic(Diagnostic.Create(PartialErr, syntax.Identifier.GetLocation(), attrName));
+
+        var isStatic = modifiers.Contains("static");
+        if (isStatic)
+            context.ReportDiagnostic(Diagnostic.Create(StaticErr, syntax.Identifier.GetLocation(), attrName));
+    } // private static void AnalyzeClass (SyntaxNodeAnalysisContext)
+} // internal sealed class AttributeUsageAnalyzer: DiagnosticAnalyzer
---
+++ DisposalGenerator/CodeFixes/RemoveStaticCodeFixProvider.cs
@@ -0,0 +1,68 @@
+﻿
+// (c) 2025 Kazuki Kohzuki
+
+using DisposalGenerator.Analyzers;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using System.Collections.Immutable;
+using System.Composition;
+
+namespace DisposalGenerator.CodeFixes;
+
+/// <summary>
+/// Provides a code fix for removing the static modifier from a class declaration.
+/// </summary>
+[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(RemoveStaticCodeFixProvider))]
+[Shared]
+internal sealed class RemoveStaticCodeFixProvider : CodeFixProvider
+{
+    override public ImmutableArray<string> FixableDiagnosticIds
+        => [AttributeUsageAnalyzer.StaticErrId];
+
+    override public FixAllProvider GetFixAllProvider()
+            => WellKnownFixAllProviders.BatchFixer;
+
+    override public async Task RegisterCodeFixesAsync(CodeFixContext context)
+    {
+        var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+        if (root is null) return;
+
+        var diagnostic = context.Diagnostics.First();
+        var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+        var nodes = root.FindToken(diagnosticSpan.Start).Parent?.AncestorsAndSelf().OfType<ClassDeclarationSyntax>();
+        if (nodes is null) return;
+        if (!nodes.Any()) return;
+        var node = nodes.First();
+
+        context.RegisterCodeFix(
+            CodeAction.Create(
+                title: "Remove static modifier",
+                createChangedDocument: c => RemoveStaticModifier(context.Document, node, c),
+                equivalenceKey: nameof(RemoveStaticCodeFixProvider)
+            ),
+            diagnostic
+        );
+    } // override public Task RegisterCodeFixesAsync (CodeFixContext)
+
+
+    /// <summary>
+    /// Removes the static modifier from the class declaration.
+    /// </summary>
+    /// <param name="document">The document to be modified.</param>
+    /// <param name="classDeclarationSyntax">The class declaration syntax to be modified.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The modified document.</returns>
+    private static async Task<Document> RemoveStaticModifier(Document document, ClassDeclarationSyntax classDeclarationSyntax, CancellationToken cancellationToken)
+    {
+        var modifiers = classDeclarationSyntax.Modifiers;
+        if (!modifiers.Any(m => m.IsKind(SyntaxKind.StaticKeyword))) return document;
+
+        var oldRoot = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+        if (oldRoot is null) return document;
+
+        var newDeclaration = classDeclarationSyntax.ReplaceToken(modifiers.First(m => m.IsKind(SyntaxKind.StaticKeyword)), SyntaxFactory.Token(SyntaxKind.None));
+        var newRoot = oldRoot.ReplaceNode(classDeclarationSyntax, newDeclaration.WithLeadingTrivia(classDeclarationSyntax.GetLeadingTrivia()));
+        return document.WithSyntaxRoot(newRoot);
+    } // private static async Task<Document> RemoveStaticModifier (Document, ClassDeclarationSyntax, CancellationToken)
+} // internal sealed class RemoveStaticCodeFixProvider : CodeFixProvider
