85e13a75c94fe36822edf81e5123e7fbf41252d0
Kazuki KOHZUKI <k.kohzuki@gmail.com>
2025-02-14 17:15:39 +0900

    Specifi element names of FilterConfig

--- TAFitting/Config/FilterConfig.cs
+++ TAFitting/Config/FilterConfig.cs
@@ -1,6 +1,7 @@
 ﻿
 // (c) 2025 Kazuki Kohzuki
 
+using System.Xml.Serialization;
 using TAFitting.Filter.SavitzkyGolay;
 
 namespace TAFitting.Config;
@@ -14,16 +15,19 @@ public sealed class FilterConfig
     /// <summary>
     /// Gets or sets the default filter.
     /// </summary>
+    [XmlElement("default-filter")]
     public Guid DefaultFilter { get; set; } = typeof(SavitzkyGolayFilterCubic25).GUID;
 
     /// <summary>
     /// Gets or sets the auto-apply flag.
     /// </summary>
+    [XmlElement("auto-apply")]
     public bool AutoApply { get; set; } = true;
 
     /// <summary>
     /// Gets or sets a value indicating whether to hide the original data.
     /// </summary>
+    [XmlElement("hide-original")]
     public bool HideOriginal { get; set; } = false;
 
     /// <summary>
