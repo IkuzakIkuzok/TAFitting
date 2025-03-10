4d197fa6822c4274c96b80067d3ff7d3ddbfad92
Kazuki KOHZUKI <k.kohzuki@gmail.com>
2025-03-10 18:54:04 +0900

    Refactor mathematical utilities and clean up code
    Updated `AdditionalCode` to include `TAFitting.Data` and replaced references to `MathUtil` with `MathUtils` in the `Generate` method. Removed unused commented-out code and eliminated the `MathUtil` class, which contained mathematical utility functions, indicating a significant change in the handling of mathematical operations.

--- TAFitting.ModelGenerator/Generators/ExponentialGenerator.cs
+++ TAFitting.ModelGenerator/Generators/ExponentialGenerator.cs
@@ -15,7 +15,7 @@ internal sealed class ExponentialGenerator : ModelGeneratorBase
 
     override protected string FileName => "GeneratedExponentialModels.g.cs";
 
-    override protected string AdditionalCode => MathUtil;
+    override protected string AdditionalCode => @"using TAFitting.Data;";
 
     override protected string Generate(string nameSpace, string className, int n, string? name)
     {
@@ -94,7 +94,7 @@ internal sealed class ExponentialGenerator : ModelGeneratorBase
         }
         builder.AppendLine();
         builder.AppendLine("\t\t\treturn x => a0"
-            + string.Concat(Enumerable.Range(1, n).Select(i => $" + a{i} * MathUtil.FastExp(x * t{i})")) + ";");
+            + string.Concat(Enumerable.Range(1, n).Select(i => $" + a{i} * MathUtils.FastExp(x * t{i})")) + ";");
         builder.AppendLine("\t\t} // public Func<double, double> GetFunction(IReadOnlyList<double> parameters)");
 
         GenerateGetVectorizedFunc(builder, "AvxVector", n);
@@ -118,7 +118,7 @@ internal sealed class ExponentialGenerator : ModelGeneratorBase
         builder.AppendLine("\t\t\t{");
         for (var i = 1; i <= n; i++)
         {
-            builder.AppendLine($"\t\t\t\tvar exp{i} = MathUtil.FastExp(x * t{i});");
+            builder.AppendLine($"\t\t\t\tvar exp{i} = MathUtils.FastExp(x * t{i});");
         }
         builder.AppendLine();
         builder.AppendLine("\t\t\t\tvar d_a0 = 1.0;");
@@ -196,99 +196,4 @@ internal sealed class ExponentialGenerator : ModelGeneratorBase
         }
         builder.AppendLine("\t\t\t};");
     } // private static void GenerateGetVectorizedDerivatives (StringBuilder, string, int)
-
-    /// <summary>
-    /// Generates the mask of the specified number of bits.
-    /// </summary>
-    /// <param name="n">The number of bits.</param>
-    /// <returns>The 64-bit mask of the specified number of bits.</returns>
-    private static ulong Mask64(int n) => (1UL << n) - 1;
-
-    private const int TABLE_SIZE = 11;
-    private const int s = 1 << TABLE_SIZE;
-
-    /// <summary>
-    /// Computes the table value at the specified index.
-    /// </summary>
-    /// <param name="index">The index of the table.</param>
-    /// <returns>The table value at the specified index.</returns>
-    private static ulong ComputeTable(int index)
-    {
-        var du = new BitsConverter64 { Double = Math.Pow(2, index * (1.0 / s)) };
-        return du.UInt64 & Mask64(52);
-    } // private static ulong ComputeTable (int)
-
-    /// <summary>
-    /// A union to convert between <see cref="double"/>, <see cref="ulong"/>, and <see cref="long"/>.
-    /// </summary>
-    [StructLayout(LayoutKind.Explicit)]
-    private struct BitsConverter64
-    {
-        /// <summary>
-        /// A <see cref="double"/> value.
-        /// </summary>
-        [FieldOffset(0)]
-        public double Double;
-
-        /// <summary>
-        /// A <see cref="ulong"/> value.
-        /// </summary>
-        [FieldOffset(0)]
-        public ulong UInt64;
-
-        /// <summary>
-        /// A <see cref="long"/> value.
-        /// </summary>
-        [FieldOffset(0)]
-        public long Int64;
-    } // private struct BitsConverter64
-
-    private static IEnumerable<string> GenerateElements(int n, int elementsInLine, Func<int, string> func, string separator)
-    {
-        for (var i = 0; i < n; i += elementsInLine)
-        {
-            var start = i;
-            var end = Math.Min(i + elementsInLine, n);
-            var elements = string.Join(separator, Enumerable.Range(start, end - start).Select(func));
-            yield return elements;
-        }
-    } // private static IEnumerable<string> GenerateElements (int, int, Func<int, string>, string)
-
-    private static readonly string MathUtil = @$"
-using System.Runtime.CompilerServices;
-using TAFitting.Data;
-
-/// <summary>
-/// Provides mathematical utility functions.
-/// </summary>
-file static class MathUtil
-{{
-    private static readonly ulong[] table = [
-        {string.Join(",\n\t\t", GenerateElements(s, 8, i => ComputeTable(i).ToString() + "UL", ", "))}
-    ];
-
-    /// <summary>
-    /// Computes the exponential function of the specified number.
-    /// </summary>
-    /// <param name=""x"">The number to compute the exponential function.</param>
-    /// <returns>The exponential function of the specified number.</returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    internal static double FastExp(double x)
-    {{
-        if (x <= {Math.Log(Math.Pow(2, -1022))}) return 0;
-        if (x >= {Math.Log(double.MaxValue)}) return double.PositiveInfinity;
-
-        var d = x * ({2048 / Math.Log(2)}) + ({3UL << 51});
-        var i = BitConverter.DoubleToUInt64Bits(d);
-        var iax = table[i & 2047];
-        var t = (d - ({3UL << 51})) * {Math.Log(2) / 2048} - x;
-        var u = ((i + {(1UL << (TABLE_SIZE + 10)) - (1UL << TABLE_SIZE)}) >> 11) << 52;
-        var y = (3.0000000027955394 - t) * (t * t) * 0.16666666685227835 - t + 1;
-        i = u | iax;
-        d = BitConverter.UInt64BitsToDouble(i);
-
-        return d * y;
-    }} // internal static double FastExp (double)
-}} // file static class MathUtil
-";
 } // internal sealed class ExponentialGenerator : ISourceGenerator
