40da7b4e75fd97cc206715f0f069d632bdcc8327
Kazuki KOHZUKI <k.kohzuki@gmail.com>
2025-05-19 08:21:51 +0900

    Enhance EquivalentSIMDAttribute with constructor and docs
    Updated the `EquivalentSIMDAttribute` class to include a constructor that initializes the `SIMDType` property directly from a `Type?` parameter. Added XML documentation comments for better clarity and understanding of the class and its constructor.

--- TAFitting.Filter/EquivalentSIMDAttribute.cs
+++ TAFitting.Filter/EquivalentSIMDAttribute.cs
@@ -3,26 +3,21 @@
 
 namespace TAFitting.Filter;
 
+/// <summary>
+/// Initializes a new instance of the <see cref="EquivalentSIMDAttribute"/> class.
+/// </summary>
+/// <param name="simdType">The equivalent SIMD type.</param>
 [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
-public sealed class EquivalentSIMDAttribute : Attribute
+public sealed class EquivalentSIMDAttribute(Type? simdType) : Attribute
 {
     /// <summary>
     /// Gets the equivalent SIMD type.
     /// </summary>
     /// <value>The equivalent SIMD type, or <see langword="null"/> if not available.</value>
-    public Type? SIMDType { get; }
+    public Type? SIMDType { get; } = simdType;
 
     /// <summary>
     /// Gets or sets the SIMD requirements.
     /// </summary>
     public SIMDRequirements SIMDRequirements { get; set; } = SIMDRequirements.None;
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="EquivalentSIMDAttribute"/> class.
-    /// </summary>
-    /// <param name="simdType">The equivalent SIMD type.</param>
-    public EquivalentSIMDAttribute(Type? simdType)
-    {
-        this.SIMDType = simdType;
-    } // ctor (Type?)
 } // public sealed class EquivalentSIMDAttribute : Attribute
