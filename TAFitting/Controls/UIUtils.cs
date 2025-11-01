d35d957378c696f68b2f81fb6acf29d900f7241b
Kazuki KOHZUKI <k.kohzuki@gmail.com>
2025-11-01 13:14:11 +0900

    Remove unused System.Runtime.CompilerServices import
    The `using System.Runtime.CompilerServices;` directive was removed from `UIUtils.cs` as it is no longer required.

--- TAFitting/Controls/UIUtils.cs
+++ TAFitting/Controls/UIUtils.cs
@@ -1,7 +1,6 @@
 ﻿
 // (c) 2024 Kazuki Kohzuki
 
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows.Forms.DataVisualization.Charting;
