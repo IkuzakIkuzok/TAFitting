bb1e624df1a9b47789f125008c0e81cdb6ffe76f
Kazuki KOHZUKI <k.kohzuki@gmail.com>
2025-11-14 17:07:56 +0900

    Add XML docs
    Added XML documentation to the `ModelItem` class, including
    descriptions for the class, its properties, and its constructor
    to enhance code readability.

--- TAFitting/Model/ModelItem.cs
+++ TAFitting/Model/ModelItem.cs
@@ -5,13 +5,27 @@ using System.Diagnostics;
 
 namespace TAFitting.Model;
 
+/// <summary>
+/// Represents an item of fitting model with its category.
+/// </summary>
 [DebuggerDisplay("[{Category,nq}] {Model.Name,nq}")]
 internal sealed class ModelItem
 {
+    /// <summary>
+    /// Gets the fitting model.
+    /// </summary>
     internal IFittingModel Model { get; }
 
+    /// <summary>
+    /// Gets the category of the fitting model.
+    /// </summary>
     internal string Category { get; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ModelItem"/> class.
+    /// </summary>
+    /// <param name="model">The fitting model.</param>
+    /// <param name="category">The category of the fitting model.</param>
     internal ModelItem(IFittingModel model, string category)
     {
         this.Model = model;
