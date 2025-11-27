5f908461bd39c80a03ee153e6b5fde9d9be65ce5
Kazuki KOHZUKI <k.kohzuki@gmail.com>
2025-11-27 14:59:39 +0900

    Add XML docs to  SyncSpectraSource record struct
    XML documentation comments were added to describe the purpose of the struct and its parameters.

--- TAFitting/Controls/Spectra/SyncSpectraSource.cs
+++ TAFitting/Controls/Spectra/SyncSpectraSource.cs
@@ -3,4 +3,9 @@
 
 namespace TAFitting.Controls.Spectra;
 
+/// <summary>
+/// Represents a synchronous source for spectra data, identified by host name and spectra ID.
+/// </summary>
+/// <param name="HostName">The name of the host providing the spectra data.</param>
+/// <param name="SpectraId">The unique identifier for the spectra source on the specified host.</param>
 internal record struct SyncSpectraSource(string HostName, int SpectraId);
