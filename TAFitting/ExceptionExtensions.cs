2ef8ef3a1e37ccbe92e87ea0198951227dd445e6
Kazuki KOHZUKI <k.kohzuki@gmail.com>
2025-12-11 11:45:37 +0900

    Add ThrowIf for InvalidOperationException to ExceptionExtensions
    Introduce internal static ThrowIf method to throw InvalidOperationException when a specified condition is true. Includes XML documentation for usage and parameters.

--- TAFitting/ExceptionExtensions.cs
+++ TAFitting/ExceptionExtensions.cs
@@ -18,4 +18,19 @@ internal static class ExceptionExtensions
                 throw new IndexOutOfRangeException($"Index {index} is out of range for {length}.");
         } // internal static void ThorwIfIndexOutOfRange (int, int)
     } // extension (IndexOutOfRangeException)
+
+    extension (InvalidOperationException)
+    {
+        /// <summary>
+        /// Throws an <see cref="InvalidOperationException"/> with the specified message if the given condition is true.
+        /// </summary>
+        /// <param name="condition">The condition to evaluate. If <see langword="true"/>, an exception is thrown.</param>
+        /// <param name="message">The exception message to use if the condition is met.</param>
+        /// <exception cref="InvalidOperationException">Thrown when <paramref name="condition"/> is <see langword="true"/>.</exception>
+        internal static void ThrowIf(bool condition, string message)
+        {
+            if (condition)
+                throw new InvalidOperationException(message);
+        } // internal static void ThrowIf (bool, string)
+    }
 } // internal static class ExceptionExtensions
